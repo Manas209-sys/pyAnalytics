9168d481b910d05474d82b9e29c56be537f117b4
Manas209-sys <85947683+Manas209-sys@users.noreply.github.com>
2021-06-18 20:53:11 +0530

    18-06-2021

--- daily1.py
+++ daily1.py
@@ -2,7 +2,7 @@
 #-----------------------------
 #Data Structures
 #List - ordered collection of items, mutable(changeable) : [square ]
-list1 = [1,2,3,4,5,'a','Dhiraj',"Upadhyaya",True]  #list type of object with data
+list1 = [1,2,3,4,5,'a','Manas',"Mishra",True]  #list type of object with data
 list1  #print when through spyder
 type(list1)  #type of object
 print(list1)  #print when running complete file
@@ -195,8 +195,10 @@ np3.reshape((-1,1))  #1 column from 2 column
 
 import pandas as pd
 pd?
-dir(pd)
+help(pd)
 
+dir(pd)
+help(pd)
 df1 = pd.DataFrame({'rollno':[1,2,3,4], 'name': [ "Dhiraj", "Kounal", "Akhil", "Pooja" ], 'marks':[ 40, 50, 60.5, 70 ], 'gender':['M', 'M','M', 'F']})
 df1
 type(df1) 
@@ -220,6 +222,7 @@ df1.groupby('gender').size().plot(kind='bar')
 plt.hist(df1['marks'])
 
 #https://seaborn.pydata.org/index.html
+
 import seaborn as sns
 # sns.set(style="ticks", color_codes=True)
 iris = sns.load_dataset("iris")
@@ -232,7 +235,7 @@ sns.pairplot(iris)  #relationship diagrams
 
 #%% #Load Inbuilt Datasets
 
-#pip install pydataset
+pip install pydataset
 from pydataset import data
 data('iris')
 data('mtcars')
